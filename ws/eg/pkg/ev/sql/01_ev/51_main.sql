7c382674550878e4b8db62b0c9b462500bd9ef43
Askar Timirgazin <AnotherOneAckap@gmail.com>
2013-03-15 12:27:40 +0400

    EV prototype Добавлен тест и исправлено расположение DDL скриптов

--- ws/eg/pkg/ev/sql/01_ev/51_main.sql
+++ ws/eg/pkg/ev/sql/01_ev/51_main.sql
@@ -128,39 +128,6 @@ $_$
 $_$;
 SELECT pg_c('f', 'role_signup_del', 'Удаление подписки роли');
 
-CREATE OR REPLACE FUNCTION ev.tr_send_notifications()
-  RETURNS trigger LANGUAGE 'plpgsql' AS
-$_$
-  DECLARE
-    v_account_id ws.d_id32; 
-    v_role_id ws.d_id32;  
-  BEGIN
-    -- определяем список адресатов события
-    FOR v_account_id, v_role_id IN SELECT a.id, r.id FROM wsd.account a 
-      JOIN ev.account_role ar ON ar.account_id = a.id 
-      JOIN ev.role r ON r.id = ar.role_id
-      JOIN wsd.event_role_signup ers ON ers.role_id = ar.role_id AND ers.kind_id = NEW.kind_id
-      -- specification
-      JOIN wsd.event_spec es ON es.event_id = NEW.id AND es.spec_id = ers.spec_id LOOP
-      -- создаём системные уведомления
-      INSERT INTO wsd.event_notify ( event_id, account_id, role_id, cause_id )
-        VALUES ( NEW.id, v_account_id, v_role_id, 1 );
-    END LOOP;
-    -- TODO меняем статус события?
-    -- TODO создаём задания на рассылку уведомлений согласно формату уведомлений
-    RETURN NEW;
-  END;
-$_$;
-
-CREATE OR REPLACE FUNCTION ev.tr_fire_user_login()
-  RETURNS trigger LANGUAGE 'plpgsql' AS
-$_$
-  BEGIN
-    PERFORM ev.fire_user_login( NEW.account_id );
-    RETURN NEW;
-  END;
-$_$;
-
 CREATE OR REPLACE FUNCTION ev.create_user_login( a_user_id ws.d_id32 )
   RETURNS wsd.event LANGUAGE 'sql' AS
 $_$
---
+++ ws/eg/pkg/ev/sql/01_ev/90_login.sql
@@ -0,0 +1,29 @@
+-- Подписать роль 1 на событие 1 со специфкацией 0
+SELECT ev.role_signup_ins( 1, 1, 0 );
+-- Сэмулировать логин вставкой в таблицу сессий
+INSERT INTO wsd.session (  account_id, role_id, ip, is_ip_checked ) VALUES
+  ( 1, 1, '127.0.0.1', false )
+;
+-- Найти новое уведомление у аккаунта 1
+SELECT ev.new_notifications_count( 1 );
+SELECT COUNT(*) FROM ev.notifications_list( 1 );
+-- Подписать роль 1 на событие 1 со специфкацией 1
+SELECT ev.role_signup_ins( 1, 1, 1 );
+-- Создать юзера с аккаунтом pro_
+INSERT INTO wsd.account ( status_id, def_role_id, login, psw, name ) VALUES 
+  ( acc.const_status_id_active(), 1, 'pro_admin', 'pro_admin', 'pro_admin' )
+;
+-- Сэмулировать логин юзера pro_ вставкой в таблицу сессий
+INSERT INTO wsd.session (  account_id, role_id, ip, is_ip_checked ) VALUES
+  ( ( SELECT id FROM wsd.account WHERE login = 'pro_admin' ), 1, '127.0.0.1', false )
+;
+-- Найти новое уведомление
+SELECT ev.new_notifications_count( 1 );
+SELECT COUNT(*) FROM ev.notifications_list( 1 );
+-- Удалить подписку
+SELECT ev.role_signup_del( 1, 1, 1 );
+SELECT ev.role_signup_del( 1, 1, 0 );
+-- Убраться за собой
+TRUNCATE wsd.event_notify;
+TRUNCATE wsd.session;
+DELETE FROM wsd.account WHERE login = 'pro_admin' AND psw = 'pro_admin';
