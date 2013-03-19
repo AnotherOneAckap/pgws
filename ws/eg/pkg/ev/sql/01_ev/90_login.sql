8857456f4ff1bf60c225fd90fca2ddb7c8b1421f
Askar Timirgazin <AnotherOneAckap@gmail.com>
2013-03-19 13:58:29 +0400

    Ev prototype Улушены тесты, добавлена проверка наличия пакета перед удалением

--- ws/eg/pkg/ev/sql/01_ev/90_login.sql
+++ ws/eg/pkg/ev/sql/01_ev/90_login.sql
@@ -1,10 +1,9 @@
 SELECT ws.test('user_login_spec_0');
 -- Подписать роль 1 на событие 1 со специфкацией 0
 SELECT ev.role_signup_ins( 1, 1, 0 );
--- Сэмулировать логин вставкой в таблицу сессий
-INSERT INTO wsd.session (  account_id, role_id, ip, is_ip_checked ) VALUES
-  ( 1, 1, '127.0.0.1', false )
-;
+-- Сэмулировать логин
+SELECT account_name FROM acc.login( '127.0.0.1', 'admin', 'pgws');
+
 -- Найти новое уведомление у аккаунта 1
 SELECT ev.new_notifications_count( 1 );
 SELECT COUNT(*) FROM ev.notifications_list( 1 );
@@ -16,10 +15,9 @@ SELECT ev.role_signup_ins( 1, 1, 1 );
 INSERT INTO wsd.account ( status_id, def_role_id, login, psw, name ) VALUES 
   ( 4, 1, 'pro_admin', 'pro_admin', 'pro_admin' )
 ;
--- Сэмулировать логин юзера pro_ вставкой в таблицу сессий
-INSERT INTO wsd.session (  account_id, role_id, ip, is_ip_checked ) VALUES
-  ( ( SELECT id FROM wsd.account WHERE login = 'pro_admin' ), 1, '127.0.0.1', false )
-;
+-- Сэмулировать логин юзера pro_
+SELECT account_name FROM acc.login( '127.0.0.1', 'pro_admin', 'pro_admin');
+
 -- Найти новое уведомление
 SELECT ev.new_notifications_count( 1 );
 SELECT COUNT(*) FROM ev.notifications_list( 1 );
